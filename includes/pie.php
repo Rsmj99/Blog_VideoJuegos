dcc4c29cf398a1deb86edf9f2783be54d9c4be74
Roger Muguruza <rogermuguruza1999@gmail.com>
2021-06-30 04:03:12 -0500

    Primer commit del proyecto completo

---
+++ includes/pie.php
@@ -0,0 +1,9 @@
+        <div class="clearfix"></div>
+        </div>
+
+<!-- PIE DE PAGINA -->      
+        <footer id="pie">
+            <p>Desarrollado por Roger Muguruza &copy; 2020</p>
+        </footer>
+    </body>
+</html>
